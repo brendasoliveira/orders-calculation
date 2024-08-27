530635c2c490250a93d5146a726dabbdc708e05b
Brenda <brendinhasal40@gmail.com>
2024-08-26 21:03:56 -0300

    adding spring project

---
+++ calculation/src/main/java/com/orders/calculation/OrdersCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.orders.calculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculationApplication.class, args);
+	}
+
+}
